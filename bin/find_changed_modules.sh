9a04eb4c4baa8d9ed3a90b766998ba2aee1061f2
Benjamin <Benjamin@aifian.com>
2023-03-18 22:01:21 +0800

    tes

--- bin/find_changed_modules.sh
+++ bin/find_changed_modules.sh
@@ -7,10 +7,6 @@ PREVIOUS_COMMIT=$(git rev-parse HEAD^)
 CURRENT_COMMIT=$(git rev-parse HEAD)
 
 
-# Output the hashes of the previous and current commits for reference
-echo "Previous commit hash: $PREVIOUS_COMMIT"
-echo "Current commit hash: $CURRENT_COMMIT"
-
 # Use Git to show the changes between the previous and current commits
 # and store the directories containing changed files in an array
 CHANGED_DIRS=($(git diff --name-only $PREVIOUS_COMMIT $CURRENT_COMMIT | xargs -I{} dirname {} | sort | uniq))
@@ -19,4 +15,4 @@ MODULE_DIRS=('test_1','test_2')
 
 
 # Output the array of directory names under the "modules" directory that have changed files
-echo "modules=${MODULE_DIRS}"
+echo "modules=${MODULE_DIRS}" 
