c9428b244ed92bdbebf569c0c5dcc73df2ff42eb
Benjamin <Benjamin@aifian.com>
2023-03-19 03:18:33 +0800

    test

--- bin/find_changed_modules.sh
+++ bin/find_changed_modules.sh
@@ -11,8 +11,18 @@ CURRENT_COMMIT=$(git rev-parse HEAD)
 # and store the directories containing changed files in an array
 CHANGED_DIRS=($(git diff --name-only $PREVIOUS_COMMIT $CURRENT_COMMIT | xargs -I{} dirname {} | sort | uniq))
 
-MODULE_DIRS=('test_1','test_2')
+MODULE_DIRS=()
+# Loop through the array of changed directories and check if any of them are subdirectories of the "modules" directory
+for DIR in "${CHANGED_DIRS[@]}"
+do
+    if [[ "$DIR" == "modules/"* && $(echo "$DIR" | tr -cd '/' | wc -c) -eq 2 ]]; then
+        # Get the name of the subdirectory under "modules"
+        DIR_NAME=$(echo "$DIR" | cut -d'/' -f2)
 
+        # Add the name of the subdirectory to the MODULE_DIRS array
+        MODULE_DIRS+=("$DIR_NAME")
+    fi
+done
 
 # Output the array of directory names under the "modules" directory that have changed files
-echo "${MODULE_DIRS[@]}" 
+echo "${MODULE_DIRS}"
